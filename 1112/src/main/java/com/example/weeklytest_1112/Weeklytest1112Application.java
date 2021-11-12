d23074c081dc1db7582f1fef09bef0537fc8dbe2
burninghering <37091602+burninghering@users.noreply.github.com>
2021-11-12 19:01:05 +0900

    1112

---
+++ 1112/src/main/java/com/example/weeklytest_1112/Weeklytest1112Application.java
@@ -0,0 +1,13 @@
+package com.example.weeklytest_1112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weeklytest1112Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Weeklytest1112Application.class, args);
+    }
+
+}
---
+++ 1112/src/test/java/com/example/weeklytest_1112/Weeklytest1112ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weeklytest_1112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Weeklytest1112ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
